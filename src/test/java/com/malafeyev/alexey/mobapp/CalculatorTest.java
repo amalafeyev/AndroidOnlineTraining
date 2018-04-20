971b103966470a688edc20b57bcc727f65f5694f
amalafeyev <alexeymalafeyev@gmail.com>
2018-04-20 18:22:59 -0400

    3.2 Testing your app:             Run the initial tests in the SimpleCalc app.             Add more tests to the SimpleCalc app.             Run those unit tests the see the results.
                Challenge 1: Dividing by zero is always worth testing for, because it a special case in arithmetic. If you try to divide by zero in the current version of the SimpleCalc app, it behaves the way Java defined: Dividing a number by returns the "Infinity" constant (Double.POSITIVE_INFINITY). Dividing 0 by 0 returns the not a number constant (Double.NaN). Although these values are correct for Java, they're not necessarily useful values for the user in the app itself. How might you change the app to more gracefully handle divide by zero? To accomplish this challenge, start with the test first -- consider what the right behavior is, and then write the tests as if that behavior already existed. Then change or add to the code so that it makes the tests come up green.

---
+++ src/test/java/com/malafeyev/alexey/mobapp/CalculatorTest.java
@@ -0,0 +1,115 @@
+package com.malafeyev.alexey.mobapp;
+
+
+/*
+ * Copyright 2016, Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * JUnit4 unit tests for the calculator logic. These are local unit tests; no device needed
+ */
+@RunWith(JUnit4.class)
+@SmallTest
+public class CalculatorTest {
+
+    private Calculator mCalculator;
+
+    /**
+     * Set up the environment for testing
+     */
+    @Before
+    public void setUp() {
+        mCalculator = new Calculator();
+    }
+
+    /**
+     * Test for simple addition
+     */
+    @Test
+    public void addTwoNumbers() {
+        double resultAdd = mCalculator.add(1d, 1d);
+        assertThat(resultAdd, is(equalTo(2d)));
+    }
+
+    @Test
+    public void addTwoNumbersNegative() {
+        double resultAdd = mCalculator.add(-1d, 2d);
+        assertThat(resultAdd, is(equalTo(1d)));
+    }
+
+    @Test
+    public void addTwoNumbersFloats() {
+        double resultAdd = mCalculator.add(1.111f, 1.111d);
+        //assertThat(resultAdd, is(closeTo(2.222, 0.01)));
+        assertThat(resultAdd, is(CoreMatchers. equalTo(2.222d)));
+    }
+
+    @Test
+    public void subTwoNumbers() {
+        double resultAdd = mCalculator.sub(1d, 1d);
+        assertThat(resultAdd, is(equalTo(0d)));
+    }
+
+    @Test
+    public void subTwoNumbersNegative() {
+        double resultAdd = mCalculator.sub(-1d, 2d);
+        assertThat(resultAdd, is(equalTo(-3d)));
+    }
+
+    @Test
+    public void mulTwoNumbers() {
+        double resultAdd = mCalculator.mul(1d, 1d);
+        assertThat(resultAdd, is(equalTo(1d)));
+    }
+
+    @Test
+    public void mulTwoNumbersNegative() {
+        double resultAdd = mCalculator.mul(-2d, 2d);
+        assertThat(resultAdd, is(equalTo(-4d)));
+    }
+
+    @Test
+    public void mulTwoNumbersZero() {
+        double resultAdd = mCalculator.mul(-2d, 0d);
+        assertThat(resultAdd, is(equalTo(-0d)));
+    }
+
+    @Test
+    public void divTwoNumbersZero() {
+        double resultAdd = mCalculator.div(-2d, 0d);
+        assertThat(resultAdd, is(equalTo(Double.NEGATIVE_INFINITY)));
+    }
+
+    @Test
+    public void divByZeroReturnsZero() {
+        double resultAdd = mCalculator.div(-2d, 0d);
+        assertThat(resultAdd, is(equalTo(0d)));
+    }
+
+
+}
